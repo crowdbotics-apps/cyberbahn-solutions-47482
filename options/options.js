aa5f7ca29c59c1bcaf15e43e8c8717b5aee81e4b
Crowdbotics <team@crowdbotics.com>
2024-04-11 22:24:59 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cyberbahn-solutions-47482",
+  url: "https://cyberbahn-solutions-47482.botics.co",
+  api: "https://cyberbahn-solutions-47482.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
